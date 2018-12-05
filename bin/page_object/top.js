6f60d6f5e25a87a46e37f22b4fd40cffbef8b1e1
Takahito Hashimoto <ta.li.ryu.1994@gmail.com>
2018-12-05 23:14:31 +0900

    [add] コンテストタイトルもデータに含める

--- bin/page_object/top.js
+++ bin/page_object/top.js
@@ -10,6 +10,7 @@ export default class Top {
     let res = [];
     $('.table > tbody > tr').each((idx, ele) => {
       res.push({
+        title: $(ele).find('td:nth-child(1)').text().trim(),
         url: $(ele).find('td:nth-child(1) > a').attr('href').trim(),
         startTime: $(ele).find('td:nth-child(2)').text().trim(),
       });

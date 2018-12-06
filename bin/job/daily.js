eb7e337bc84d78548c0123a44cb2ac7d28611ce8
Takahito Hashimoto <ta.li.ryu.1994@gmail.com>
2018-12-06 23:07:33 +0900

    [refactor] notice ジョブでページオブジェクトを使う

--- bin/job/daily.js
+++ bin/job/daily.js
@@ -1,13 +1,12 @@
-'use strict';
-import moment from 'moment';
+'use strict'
 
-import * as consts from '../consts';
+import moment from 'moment'
 
+import * as consts from '../consts'
 import Top from '../page_object/top'
 import Contest from '../page_object/contest'
-
 import Req from '../util/request-wrapper'
-import Twitter from '../util/twitter';
+import Twitter from '../util/twitter'
 
 const message = (date, top, max) =>
   `${date}のばちゃこんのAC数の記録
@@ -30,24 +29,24 @@ export default class Daily {
   }
 
   async run() {
-    const lastDay =
-      moment().utc().add(9, 'h')
+    const lastDay
+      = moment().utc().add(9, 'h')
         .subtract(1, 'd')
         .format('YYYY-MM-DD');
 
-    const contestList =
-      new Top(await Req.get(consts.PATH.top))
+    const contestList
+      = new Top(await Req.get(consts.PATH.top))
         .getContestUrlList()
         .filter(c => c.startTime.startsWith(lastDay))
 
-    const contestSummaryList =
-      (await Promise.all(contestList.map(c => Req.get(c.url))))
+    const contestSummaryList
+      = (await Promise.all(contestList.map(c => Req.get(c.url))))
         .map(body => new Contest(body))
         .map(c => c.summary())
 
-    const summaryList =
-      Array.prototype.concat.apply([],
-        contestSummaryList.map(s => Object.keys(s)))
+    const summaryList
+      = Array.prototype.concat.apply([],
+          contestSummaryList.map(s => Object.keys(s)))
         .filter((x, i, self) => self.indexOf(x) === i)
         .map(user => ({
           'userId': user,
@@ -55,6 +54,6 @@ export default class Daily {
             .map(s => s[user] || 0)
             .reduce((prev, crt, i, arr) => prev + crt)
         }))
-    Twitter.tweet(this.buildTweetMessage(summaryList));
+    Twitter.tweet(this.buildTweetMessage(summaryList))
   }
 }
